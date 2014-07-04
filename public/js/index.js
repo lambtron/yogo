89498527c845f0388356be932939e5ea845117e0
Andy Jiang <andyjiang@gmail.com>
2014-07-03 21:49:53 -0700

    added map

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,37 @@
 'use strict';
 
 (function() {
-	// Page specific JS here.	
+	// Page specific JS here.
+  var infowindow = null;
+
+  function initialize(position) {
+    var mapOptions = {
+      center: new google.maps.LatLng(position.coords.latitude,
+        position.coords.longitude),
+      zoom: 13
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+
+    // Add your location.
+    var you = new google.maps.Marker({
+      position: new google.maps.LatLng(position.coords.latitude,
+        position.coords.longitude),
+      map: map,
+      title: 'You',
+      animation: google.maps.Animation.DROP,
+      icon: '/public/img/currentlocation.png'
+    });
+  }
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(initialize);
+    } else {
+      var x = document.getElementById("map-canvas");
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  google.maps.event.addDomListener(window, 'load', getLocation);
 }());
