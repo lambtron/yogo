b4cc695a8c8e0c50eae5f27a46875bc527c0bc1c
Andy Jiang <andyjiang@gmail.com>
2014-07-04 00:34:02 -0700

    added a todo

--- config/routes.js
+++ config/routes.js
@@ -9,7 +9,7 @@
   var Go = require('../app/models/goes');
 
   // Static variables.
-  var RADIUS = 1;
+  var RADIUS = 100;
 
   // Public functions. =========================================================
   module.exports = function (app) {
@@ -21,20 +21,57 @@
       // yo_name: ''
       var lat = req.query.lat;
       var lng = req.query.lng;
+      var location = {
+        lat: lat,
+        lng: lng
+      };
+      // var latMin = req.query.lat - RADIUS;
+      // var latMax = req.query.lag + RADIUS;
+      // var lngMin = req.query.lng - RADIUS;
+      // var lngMax = req.query.lng + RADIUS;
       var target = req.query.yo_name;
 
       // Retrieve from Mongo that is close to lat and lng.
+      var point = {
+        type: "Point",
+        coordinates: [req.query.lat, req.query.lng]
+      };
+
       Go.create.find({})
-      .where('lat').gt(lat - RADIUS).lt(lat + RADIUS)
-      .where('lng').gt(lng - RADIUS).lt(lng + RADIUS)
-      .exec(function (err, goes) {
-        for (var i = 0; i < goes.length; i++) {
-          (function (target, message) {
-            // Send Yos.
-            Yo.yo(target, message);
-          })(target, goes[i].message);
-        }
-      });
+      // .where('lat').gt(latMin).lt(latMax)
+      // .where('lng').gt(lngMin).lt(lngMax)
+      .exec(function (err, data) {
+        if (getDistanceFromLatLonInKm(this.lat, this.lng, data.lat, data.lng)
+          < 100) {
+          console.log('hi');
+          for (var i = 0; i < data.length; i++) {
+            (function (target, message) {
+              // Send Yos.
+              Yo.yo(target, message);
+            })(target, data[i].message);
+          }
+        };
+      }.bind(location));
+
+      function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1);
+        var a =
+          Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon/2) * Math.sin(dLon/2)
+          ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        console.log('hi');
+        return d;
+      }
+
+      function deg2rad(deg) {
+        return deg * (Math.PI/180)
+      }
+
     });
 
     app.post('/leaveyogos', function (req, res) {
--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,13 @@
 
 (function() {
   $(document).ready( function () {
+    var lat
+      , lng;
+
     $('#yo-name-input-button').on('click', function (event) {
       var load = {
-        lat: 1,
-        lng: 1,
+        lat: lat,
+        lng: lng,
         yo_name: $('#yo-name-input').val()
       };
 
@@ -23,48 +26,30 @@
       //
       var load = {
         message: $('#message-input').val(),
-        lat: 2,
-        lng: 2
+        lat: lat,
+        lng: lng
       };
 
       $.post('/leaveyogos', load, function (data) {
         console.log(data);
       });
     });
-  })
-
-
+  });
 
 	// Page specific JS here.
-  // function initialize(position) {
-  //   var mapOptions = {
-  //     center: new google.maps.LatLng(position.coords.latitude,
-  //       position.coords.longitude),
-  //     zoom: 13
-  //   };
-  //   var map = new google.maps.Map(document.getElementById("map-canvas"),
-  //     mapOptions);
+  function initialize(position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+  }
 
-  //   console.log(map);
-  //   // Add your location.
-  //   // var you = new google.maps.Marker({
-  //   //   position: new google.maps.LatLng(position.coords.latitude,
-  //   //     position.coords.longitude),
-  //   //   map: map,
-  //   //   title: 'You',
-  //   //   animation: google.maps.Animation.DROP
-  //   //   // icon: '/public/img/currentlocation.png'
-  //   // });
-  // }
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(initialize);
+    } else {
+      var x = document.getElementById("map-canvas");
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
 
-  // function getLocation() {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(initialize);
-  //   } else {
-  //     var x = document.getElementById("map-canvas");
-  //     x.innerHTML = "Geolocation is not supported by this browser.";
-  //   }
-  // }
-
-  // google.maps.event.addDomListener(window, 'load', getLocation);
+  getLocation;
 }());
